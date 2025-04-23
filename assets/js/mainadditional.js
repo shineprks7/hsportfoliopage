0f096dba5bb81fb13d08cf9fae77296c3cd65b45
shineprks7 <prakash.p@hoostack.com>
2025-04-23 16:27:52 +0530

    changes

--- assets/js/mainadditional.js
+++ assets/js/mainadditional.js
@@ -45,3 +45,84 @@ for(let i=0;i<portfoliotabbuttons.length;i++)
 })
 
 }
+
+
+const textToSpannedHeader = document.querySelectorAll('.typable-text-header')
+
+textToSpannedHeader.forEach((el) => 
+{
+    let tcontent = el.textContent.trim();
+
+    el.innerHTML = "";
+
+    [...tcontent].forEach((char,index) => {
+        const span = document.createElement("span");
+
+
+        span.textContent = char === " "?"\u00A0":char;
+
+        span.style.setProperty("--i",index);
+
+
+        el.appendChild(span);
+
+    })
+
+
+})
+
+
+const textToSpannedParagraph = document.querySelectorAll('.typable-text-paragraph')
+
+textToSpannedParagraph.forEach((el) => 
+{
+    let tcontent = el.textContent.trim();
+
+    el.innerHTML = "";
+
+    let index = 0;
+    tcontent.split(" ").forEach((word) => {
+        const span = document.createElement("span");
+        const space = document.createElement("span");
+
+       if(word != "")
+       {
+        span.textContent = word;
+        space.textContent = " ";
+
+        span.style.setProperty("--i",index);
+
+
+        el.appendChild(span);
+        el.appendChild(space);
+
+        index++;
+       }
+
+    })
+
+
+})
+
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add("active");
+
+        }
+    });
+},{
+    threshold:0.5
+});
+
+document.querySelectorAll(".typable-text-paragraph").forEach(el => observer.observe(el));
+document.querySelectorAll(".typable-text-header").forEach(el => observer.observe(el));
+
+document.querySelectorAll(".solution-list-animation .solution-list-item").forEach(el => observer.observe(el));
+
+document.querySelectorAll(".image-reveal-animation").forEach(el => observer.observe(el));
+
+
